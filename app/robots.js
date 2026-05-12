ac68cf6cb8e386c13c5e360dc8683470f2d95d33
Badsha-Nishan <badshanisan14@gmail.com>
2026-05-12 18:32:19 +0600

    Added sitemap and robots

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://portfolio-web-badsha.vercel.app/sitemap.xml",
+  };
+}
